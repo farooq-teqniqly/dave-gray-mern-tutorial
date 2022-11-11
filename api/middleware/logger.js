cb6c3c226eeeb74ce86d023e361f83dcc195c796
Farooq Mahmud <49829654+farooq-teqniqly@users.noreply.github.com>
2022-11-11 10:05:17 -0800

    Starter API (#1)

---
+++ api/middleware/logger.js
@@ -0,0 +1,33 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message, logFileName) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+  try {
+    const logFilePath = path.join(__dirname, "..", "logs");
+
+    if (!fs.existsSync(logFilePath)) {
+      await fsPromises.mkdir(logFilePath);
+    }
+
+    await fsPromises.appendFile(path.join(logFilePath, logFileName), logItem);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  console.log(`${req.method}\t${req.path}`);
+  next();
+};
+
+module.exports = {
+  logEvents,
+  logger,
+};
