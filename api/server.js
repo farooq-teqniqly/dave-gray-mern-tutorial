48a2b8402a59f8344316fff418a0ecdb4d3f1337
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2022-11-10 22:04:29 -0800

    Middleware setup.

--- api/server.js
+++ api/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser);
+
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
+
 app.all("*", (req, res) => {
   res.status(404);
 
@@ -22,5 +34,7 @@ app.all("*", (req, res) => {
 
   res.type("txt").send("404 resource not found");
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
