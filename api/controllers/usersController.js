7a04d769fed4feff3eba991cb42c3e4d9e20a26b
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2022-11-11 22:43:31 -0800

    Refactoring

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 const Note = require("../models/Note");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const responseHelpers = require("../utils/responseHelpers");
 
 const removeFields = "-password -__v";
 
@@ -12,10 +13,10 @@ const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select(removeFields).lean();
 
   if (!users?.length) {
-    return res.status(400).json({ message: "No users found." });
+    return responseHelpers.badRequest(res, "No users found.");
   }
 
-  return res.json(users);
+  return responseHelpers.okWithContent(res, users);
 });
 
 //  @desc Create new user
@@ -25,13 +26,13 @@ const createNewUser = asyncHandler(async (req, res) => {
   const { username, password, roles } = req.body;
 
   if (!username || !password || !Array.isArray(roles) || !roles.length) {
-    return res.status(400).json({ message: "All fields are required." });
+    return responseHelpers.badRequest(res, "All fields are required.");
   }
 
   const duplicate = await User.findOne({ username }).lean().exec();
 
   if (duplicate) {
-    return res.status(409).json({ message: "Username already taken." });
+    return responseHelpers.conflict(res, "Username already taken.");
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -41,9 +42,9 @@ const createNewUser = asyncHandler(async (req, res) => {
 
   if (user) {
     const result = await User.findById(user._id).select(removeFields).lean();
-    res.status(201).json(result);
+    responseHelpers.createdWithContent(res, result);
   } else {
-    res.status(400).json({ message: "Invalid user data received." });
+    responseHelpers.badRequest(res, "Invalid user data received.");
   }
 });
 
@@ -60,19 +61,19 @@ const updateUser = asyncHandler(async (req, res) => {
     !roles.length ||
     typeof active !== "boolean"
   ) {
-    return res.status(400).json({ message: "All fields are required." });
+    return responseHelpers.badRequest(res, "All fields are required.");
   }
 
   const user = await User.findById(id).exec();
 
   if (!user) {
-    return res.status(404).json({ message: "User not found." });
+    return responseHelpers.notFound(res, "User not found.");
   }
 
   const duplicate = await User.findOne({ username }).lean().exec();
 
   if (duplicate && duplicate?._id.toString() !== id) {
-    return res.status(409).json({ message: "Username already taken." });
+    return responseHelpers.conflict(res, "Username already taken.");
   }
 
   user.user = username;
@@ -88,7 +89,7 @@ const updateUser = asyncHandler(async (req, res) => {
     .select(removeFields)
     .lean();
 
-  res.status(200).json(result);
+  responseHelpers.okWithContent(res, result);
 });
 
 //  @desc Delete a user
@@ -100,20 +101,21 @@ const deleteUser = asyncHandler(async (req, res) => {
   const notes = await Note.findOne({ user: id }).lean().exec();
 
   if (notes) {
-    return res
-      .status(400)
-      .json({ message: "Cannot delete user because it has assigned notes." });
+    return responseHelpers.badRequest(
+      res,
+      "Cannot delete user because it has assigned notes."
+    );
   }
 
   const user = await User.findById(id).exec();
 
   if (!user) {
-    return res.status(400).json({ message: `User id ${id} not found.` });
+    return responseHelpers.badRequest(res, "User not found.");
   }
 
   await user.deleteOne();
 
-  res.status(204).send();
+  responseHelpers.noContent(res);
 });
 
 module.exports = {
